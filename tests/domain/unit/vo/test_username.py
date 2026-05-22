6bffc6d9bb53e2e5c09b475ee6d0a776d92c31f2
Arman <cybertechik@protonmail.com>
2026-05-22 14:40:32 +0330

    created a test for vo username and test was successful

---
+++ tests/domain/unit/vo/test_username.py
@@ -0,0 +1,29 @@
+import pytest
+
+from domain.vo.username import UserName
+
+
+# happy paths
+def test_valid_username_and_normalization_functionality():
+    
+    username = UserName(" Arman ")
+
+    assert username._value == "arman"
+
+# failure tests using pytest parametrize decorator for each invalid inputs with their error types
+@pytest.mark.parametrize(
+    "invalid_inputs, error_type",
+    [
+        (1231, TypeError),
+        (None, TypeError),
+        ("", ValueError),
+        ("  ", ValueError),
+        ("s", ValueError),
+        ("longggggggggggggggggggggggggggggggggggggggggggggggggggg", ValueError),
+        ("Spe@ial", ValueError),
+    ]
+)
+def test_invalid_usernames(invalid_inputs, error_type):
+
+    with pytest.raises(error_type):
+        UserName(invalid_inputs)
